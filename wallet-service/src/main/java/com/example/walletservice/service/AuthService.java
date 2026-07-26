636a54b97c751fc8c23e1032563c61c5833d0ad6
AbdulrhmanWael <23p0155@eng.asu.edu.eg>
2026-07-26 14:43:12 +0300

    did create wallet

--- wallet-service/src/main/java/com/example/walletservice/service/AuthService.java
+++ wallet-service/src/main/java/com/example/walletservice/service/AuthService.java
@@ -4,12 +4,17 @@ import com.example.walletservice.dto.request.LoginRequest;
 import com.example.walletservice.dto.request.RegisterRequest;
 import com.example.walletservice.dto.response.AuthResponse;
 import com.example.walletservice.entity.User;
+import com.example.walletservice.entity.Wallet;
 import com.example.walletservice.exception.EmailAlreadyExistsException;
 import com.example.walletservice.repository.UserRepository;
+import com.example.walletservice.repository.WalletRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class AuthService {
@@ -18,14 +23,16 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final WalletRepository walletRepository;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager,WalletRepository walletRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+        this.walletRepository=walletRepository;
     }
-
+    @Transactional
     public AuthResponse register(RegisterRequest request) {
         if (userRepository.findByEmail(request.getEmail()).isPresent()) {
             throw new EmailAlreadyExistsException();
@@ -36,13 +43,22 @@ public class AuthService {
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
-        userRepository.save(user);
+
+       User saveduser = userRepository.save(user);
+
+        Wallet wallet =new Wallet();
+        wallet.setUser(saveduser);
+
+        walletRepository.save(wallet);
+
 
         String jwtToken = jwtService.generateToken(user);
 
         return new AuthResponse(jwtToken, new AuthResponse.UserDto(user.getId(), user.getFullName(), user.getEmail()));
     }
 
+
+//    LOGIN
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
