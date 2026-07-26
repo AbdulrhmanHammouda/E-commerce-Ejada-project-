8795c21a3d994ab001cf98c119589b60272ad4b0
AbdulrhmanWael <23p0155@eng.asu.edu.eg>
2026-07-26 15:50:19 +0300

    added pessimistic write

--- wallet-service/src/main/java/com/example/walletservice/repository/WalletRepository.java
+++ wallet-service/src/main/java/com/example/walletservice/repository/WalletRepository.java
@@ -1,10 +1,18 @@
 package com.example.walletservice.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import jakarta.persistence.LockModeType;
 import com.example.walletservice.entity.Wallet;
 
 public interface WalletRepository
         extends JpaRepository<Wallet, Long> {
 
     Optional<Wallet> findByUserId(Long userId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT w FROM Wallet w WHERE w.user.id = :userId")
+    Optional<Wallet> findByUserIdWithLock(@Param("userId") Long userId);
 }
